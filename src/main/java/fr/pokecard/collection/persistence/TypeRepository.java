f12114dc90210adb8972448740ff58fa7b573fda
aurelieclavier <aurelie.cissokho@gmail.com>
2020-01-15 23:26:43 +0100

    add method for search type by name

--- src/main/java/fr/pokecard/collection/persistence/TypeRepository.java
+++ src/main/java/fr/pokecard/collection/persistence/TypeRepository.java
@@ -10,5 +10,5 @@ import fr.pokecard.collection.business.entity.Type;
  */
 @Repository
 public interface TypeRepository extends JpaRepository<Type, Integer> {
-
+	Type findOneByName(final String name);
 }
