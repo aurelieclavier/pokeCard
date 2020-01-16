836221af102137f189c2be788672688d69a6d7b9
aurelieclavier <aurelie.cissokho@gmail.com>
2020-01-16 15:49:54 +0100

    define a new method for find data in API

--- src/main/java/fr/pokecard/collection/presentation/DataController.java
+++ src/main/java/fr/pokecard/collection/presentation/DataController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import fr.pokecard.collection.business.service.CardSerieService;
 import fr.pokecard.collection.business.service.CardService;
+import fr.pokecard.collection.business.service.RarityService;
 import fr.pokecard.collection.business.service.TypeService;
 
 @RestController
@@ -21,17 +23,37 @@ public class DataController {
 	@Autowired
 	private CardService cardService;
 
+	@Autowired
+	private RarityService rarityService;
+
+	@Autowired
+	private CardSerieService cardSerieService;
+
 	/**
 	 * Default constructor
 	 */
 	public DataController() {
 	}
 
-	/**
-	 *
-	 * final String url = "https://api.pokemontcg.io/v1/cards/dp6-90";
-	 *
-	 */
+	@GetMapping("data/serie")
+	public void getDataSerie() {
+		final ObjectMapper mapper = new ObjectMapper();
+		final String JSON_POKEMON_API_SERIE = "https://api.pokemontcg.io/v1/sets";
+
+		try {
+			JsonNode rootsets = mapper.readTree(new URL(JSON_POKEMON_API_SERIE));
+			JsonNode setNode = rootsets.path("sets");
+			if (setNode.isArray()) {
+				for (JsonNode set : setNode) {
+					String serie = set.findPath("series").asText();
+					this.cardSerieService.saveData(serie);
+					System.out.println(serie.toString() + "\n");
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Erreur de récupération des sets");
+		}
+	}
 
 	@GetMapping("/data/type")
 	public void getDataType() {
@@ -51,8 +73,26 @@ public class DataController {
 		}
 	}
 
+	@GetMapping("/data/rarity")
+	public void getDataRarity() {
+		final ObjectMapper mapper = new ObjectMapper();
+		final String JSON_POKEMON_API_CARDS = "https://api.pokemontcg.io/v1/cards";
+		try {
+			JsonNode rootCards = mapper.readTree(new URL(JSON_POKEMON_API_CARDS));
+			JsonNode cardNode = rootCards.path("cards");
+			if (cardNode.isArray()) {
+				for (JsonNode card : cardNode) {
+					String rarity = card.findPath("rarity").asText();
+					this.rarityService.saveData(rarity);
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Erreur de récupération des cartes");
+		}
+	}
+
 	@GetMapping("/data/supertype")
-	public JsonNode getDataSuperType() {
+	public void getDataSuperType() {
 		final ObjectMapper mapper = new ObjectMapper();
 		final String JSON_POKEMON_API_SUPERTYPES = "https://api.pokemontcg.io/v1/supertypes";
 
@@ -60,16 +100,17 @@ public class DataController {
 			JsonNode rootSuperTypes = mapper.readTree(new URL(JSON_POKEMON_API_SUPERTYPES));
 			JsonNode superTypeNode = rootSuperTypes.path("supertypes");
 
-			System.out.println("super type : " + superTypeNode);
-			return superTypeNode;
+			for (JsonNode superType : superTypeNode) {
+				System.out.println(superType.toString().substring(1, superType.toString().length() - 1));
+			}
+
 		} catch (Exception e) {
 			System.out.println("Erreur de récupération des supers types");
 		}
-		return null;
 	}
 
 	@GetMapping("/data/subtype")
-	public JsonNode getDataSubType() {
+	public void getDataSubType() {
 		final ObjectMapper mapper = new ObjectMapper();
 		final String JSON_POKEMON_API_SUBTYPES = "https://api.pokemontcg.io/v1/subtypes";
 
@@ -77,12 +118,13 @@ public class DataController {
 			JsonNode rootSubTypes = mapper.readTree(new URL(JSON_POKEMON_API_SUBTYPES));
 			JsonNode subTypeNode = rootSubTypes.path("subtypes");
 
-			System.out.println("subtype : " + subTypeNode);
-			return subTypeNode;
+			for (JsonNode subType : subTypeNode) {
+				System.out.println(subType.toString().substring(1, subType.toString().length() - 1));
+			}
+
 		} catch (Exception e) {
 			System.out.println("Erreur de récupération des sous types");
 		}
-		return null;
 	}
 
 	@GetMapping("/data/card")
@@ -95,7 +137,7 @@ public class DataController {
 			JsonNode rootCards = mapper.readTree(new URL(JSON_POKEMON_API_CARDS));
 			JsonNode cardNode = rootCards.path("cards");
 
-			System.out.println("card : " + cardNode);
+//			System.out.println("card : " + cardNode);
 
 			if (cardNode.isArray()) {
 				System.out.println("Is this node an Array? " + cardNode.isArray());
@@ -113,22 +155,24 @@ public class DataController {
 					String set = card.findPath("set").asText();
 					String setCode = card.findPath("setCode").asText();
 
-					System.out.println(
-							"*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_");
-					System.out.println("name : " + name);
-					System.out.println("pokedex number : " + pokedexNumber);
-					System.out.println("image : " + image);
-					System.out.println("evolution : " + evolution);
-					System.out.println("hp : " + hp);
-					System.out.println("number : " + number);
-					System.out.println("artist : " + artist);
-					System.out.println("rarity : " + rarity);
-					System.out.println("serie : " + serie);
-					System.out.println("set : " + set);
-					System.out.println("set code : " + setCode);
+					System.out.println(card.toString().substring(1, card.toString().length() - 1) + "\n" + "\n");
 
-					System.out.println(
-							"*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_");
+//					System.out.println(
+//							"*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_");
+//					System.out.println("name : " + name);
+//					System.out.println("pokedex number : " + pokedexNumber);
+//					System.out.println("image : " + image);
+//					System.out.println("evolution : " + evolution);
+//					System.out.println("hp : " + hp);
+//					System.out.println("number : " + number);
+//					System.out.println("artist : " + artist);
+//					System.out.println("rarity : " + rarity);
+//					System.out.println("serie : " + serie);
+//					System.out.println("set : " + set);
+//					System.out.println("set code : " + setCode);
+//
+//					System.out.println(
+//							"*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_");
 				}
 			}
 		} catch (Exception e) {
