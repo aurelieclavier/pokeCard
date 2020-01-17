8f550306bdba885a9f84ca5445d8418ba334c057
aurelieclavier <aurelie.cissokho@gmail.com>
2020-01-17 12:56:18 +0100

    add a new entity for add a card's subtype

---
+++ src/main/java/fr/pokecard/collection/business/entity/CardSubtype.java
@@ -0,0 +1,84 @@
+package fr.pokecard.collection.business.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+*
+*/
+@Entity
+@Table(name = "CARD_SUBTYPE")
+public class CardSubtype implements Serializable {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor
+	 */
+	public CardSubtype() {
+	}
+
+	/**
+	 *
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	/**
+	 *
+	 */
+	private String name;
+
+	/**
+	 *
+	 */
+	@OneToMany(mappedBy = "cardSubtype")
+	@JsonIgnore
+	private List<Card> cards = new ArrayList<>();
+
+	public CardSubtype(Integer id, String name, List<Card> cards) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cards = cards;
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Card> getCards() {
+		return this.cards;
+	}
+
+	public void setCards(List<Card> cards) {
+		this.cards = cards;
+	}
+
+}
