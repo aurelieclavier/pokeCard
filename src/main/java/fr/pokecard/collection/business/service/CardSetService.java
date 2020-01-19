38366deeb2b574ed6da1fae8a945a2d42c9cf5b5
aurelieclavier <aurelie.cissokho@gmail.com>
2020-01-19 19:37:24 +0100

    define a new method for data's recuperation

--- src/main/java/fr/pokecard/collection/business/service/CardSetService.java
+++ src/main/java/fr/pokecard/collection/business/service/CardSetService.java
@@ -1,11 +1,14 @@
 package fr.pokecard.collection.business.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fr.pokecard.collection.business.entity.CardSerie;
 import fr.pokecard.collection.business.entity.CardSet;
+import fr.pokecard.collection.persistence.CardSerieRepository;
 import fr.pokecard.collection.persistence.CardSetRepository;
 
 @Service
@@ -14,8 +17,21 @@ public class CardSetService {
 	@Autowired
 	private CardSetRepository cardSetRepository;
 
+	@Autowired
+	private CardSerieRepository cardSerieRepository;
+
 	public List<CardSet> getAll() {
 		return this.cardSetRepository.findAll();
+	}
+
+	public void saveData(String name, Integer totalCards, LocalDate releaseDate, String symbol, String logo,
+			String code, String ptcgoCode, String nameSerie) {
+		CardSerie serie = this.cardSerieRepository.findOneByName(nameSerie);
+		if (serie != null) {
+			CardSet newSet = new CardSet(name, totalCards, releaseDate, symbol, logo, code, ptcgoCode, serie);
+			this.cardSetRepository.save(newSet);
+		}
+
 	}
 
 }
