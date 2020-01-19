6a04e1c3f325ff8be76053a9b934140ff3b86613
aurelieclavier <aurelie.cissokho@gmail.com>
2020-01-19 17:46:48 +0100

    add annotation on repository

--- src/main/java/fr/pokecard/collection/persistence/CardSerieRepository.java
+++ src/main/java/fr/pokecard/collection/persistence/CardSerieRepository.java
@@ -1,12 +1,14 @@
 package fr.pokecard.collection.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.pokecard.collection.business.entity.CardSerie;
 
 /**
  *
  */
+@Repository
 public interface CardSerieRepository extends JpaRepository<CardSerie, Integer> {
 	CardSerie findOneByName(final String name);
 }
