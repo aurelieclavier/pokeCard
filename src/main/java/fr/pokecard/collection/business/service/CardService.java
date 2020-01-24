1fd244b9dcb6464b2c9073383e03b29a4cf069f4
aurelieclavier <aurelie.cissokho@gmail.com>
2020-01-24 23:50:29 +0100

    cardService updated in order to perform data validation

--- src/main/java/fr/pokecard/collection/business/service/CardService.java
+++ src/main/java/fr/pokecard/collection/business/service/CardService.java
@@ -39,6 +39,21 @@ public class CardService {
 	@Autowired
 	private CardSubtypeRepository cardSubtypeRepository;
 
+	@Autowired
+	private RarityService rarityService;
+
+	@Autowired
+	private CardTypeService cardTypeService;
+
+	@Autowired
+	private CardSubtypeService cardSubtypeService;
+
+	@Autowired
+	private CardSetService cardSetService;
+
+	@Autowired
+	private RetreatService retreatService;
+
 	public List<Card> getAll() {
 		return this.cardRepository.findAll();
 	}
@@ -47,29 +62,64 @@ public class CardService {
 		return this.cardRepository.findOneById(id);
 	}
 
-	public int saveData(String code, String setId, String name, Integer nationalPokedexNumber, String image, Integer hp,
-			Integer cardNumber, String illustrator, String rarity, String cardType, String cardSubtype, String cardSet,
-			Integer retreat) {
+	public void saveData(String code, String setId, String name, Integer nationalPokedexNumber, String image,
+			Integer hp, Integer cardNumber, String illustrator, String rarity, String cardType, String cardSubtype,
+			String cardSet, Integer retreat) {
 
 		int j = 0;
 		Card card = this.cardRepository.findOneByNameAndCode(name, code);
 
+		/*
+		 * Get data if element exist If element not exist insert in BDD
+		 */
+		// Start
 		Rarity rarityLabel = this.rarityRepository.findOneByLabel(rarity);
-		CardType cardTypeName = this.cardTypeRepository.findOneByName(cardType);
-		CardSubtype cardSubtypeName = this.cardSubtypeRepository.findOneByName(cardSubtype);
-		CardSet cardSetName = this.cardSetRepository.findOneByNameAndCode(cardSet, setId);
-		Retreat retreatValue = this.retreatRepository.findOneByValue(retreat);
+		if (rarityLabel == null) {
+			this.rarityService.saveData(rarity);
+			rarityLabel = this.rarityRepository.findOneByLabel(rarity);
+		}
 
-		if (card == null && rarityLabel != null && cardTypeName != null && cardSetName != null
-				&& retreatValue != null) {
-			j = 1;
+		CardType cardTypeName = this.cardTypeRepository.findOneByName(cardType);
+		if (cardTypeName == null) {
+			this.cardTypeService.saveData(cardType);
+			cardTypeName = this.cardTypeRepository.findOneByName(cardType);
+		}
+
+		CardSubtype cardSubtypeName = this.cardSubtypeRepository.findOneByName(cardSubtype);
+		if (cardSubtypeName == null) {
+			this.cardSubtypeService.saveData(cardSubtype);
+			cardSubtypeName = this.cardSubtypeRepository.findOneByName(cardSubtype);
+		}
+
+		Retreat retreatValue = this.retreatRepository.findOneByValue(retreat);
+		if (retreatValue == null) {
+			this.retreatService.saveData(retreat);
+			retreatValue = this.retreatRepository.findOneByValue(retreat);
+		}
+		// End
+
+		CardSet cardSetName = this.cardSetRepository.findOneByNameAndCode(cardSet, setId);
+
+		if (card == null) {
 			Card newCard = new Card(code, name, nationalPokedexNumber, image, hp, cardNumber, illustrator, rarityLabel,
 					cardTypeName, cardSubtypeName, cardSetName, retreatValue);
 			this.cardRepository.save(newCard);
 		} else {
+			System.out.println("----------------------------------------------------");
 			System.out.println(code + " code");
 			System.out.println(name + " name");
+			System.out.println(setId + " setId");
+			System.out.println(nationalPokedexNumber + "nationalPokedexNumber");
+			System.out.println(image + " image");
+			System.out.println(hp + " hp");
+			System.out.println(cardNumber + " cardNumber");
+			System.out.println(illustrator + " illustrator");
+			System.out.println(rarity + " rarity");
+			System.out.println(cardType + " cardType");
+			System.out.println(cardSubtype + " cardSubtype");
+			System.out.println(cardSet + " cardSet");
+			System.out.println(retreat + " retreat");
+			System.out.println("----------------------------------------------------");
 		}
-		return j;
 	}
 }
