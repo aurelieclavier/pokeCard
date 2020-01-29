97843593adde84ce8e91ec1c93986bca8355371c
aurelieclavier <aurelie.cissokho@gmail.com>
2020-01-29 13:02:40 +0100

    define a new method for save data resistance has type in bdd

--- src/main/java/fr/pokecard/collection/business/service/ResistanceService.java
+++ src/main/java/fr/pokecard/collection/business/service/ResistanceService.java
@@ -6,13 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.pokecard.collection.business.entity.Resistance;
+import fr.pokecard.collection.business.entity.Type;
 import fr.pokecard.collection.persistence.ResistanceRepository;
+import fr.pokecard.collection.persistence.TypeRepository;
 
 @Service
 public class ResistanceService {
 
 	@Autowired
 	private ResistanceRepository resistanceRepository;
+
+	@Autowired
+	private TypeRepository typeRepository;
 
 	public List<Resistance> getAll() {
 		return this.resistanceRepository.findAll();
@@ -23,6 +28,18 @@ public class ResistanceService {
 		if (resistance == null) {
 			Resistance newResistance = new Resistance(rate);
 			this.resistanceRepository.save(newResistance);
+		}
+	}
+
+	public void saveResistanceHasType(String rate, String name) {
+		Resistance resistance = this.resistanceRepository.findOneByRate(rate);
+		Type type = this.typeRepository.findOneByName(name);
+
+		if (resistance != null && type != null) {
+			if (!resistance.getTypesResistance().contains(type)) {
+				resistance.getTypesResistance().add(type);
+				this.resistanceRepository.save(resistance);
+			}
 		}
 	}
 }
