7158af5e5b3021a7b15ae3041c9c59b8b76dc620
aurelieclavier <aurelie.cissokho@gmail.com>
2020-01-29 21:43:10 +0100

    define a new method for save data in BDD

--- src/main/java/fr/pokecard/collection/business/service/WeaknessService.java
+++ src/main/java/fr/pokecard/collection/business/service/WeaknessService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fr.pokecard.collection.business.entity.Type;
 import fr.pokecard.collection.business.entity.Weakness;
+import fr.pokecard.collection.persistence.TypeRepository;
 import fr.pokecard.collection.persistence.WeaknessRepository;
 
 @Service
@@ -13,6 +15,9 @@ public class WeaknessService {
 
 	@Autowired
 	private WeaknessRepository weaknessRepository;
+
+	@Autowired
+	private TypeRepository typeRepository;
 
 	public List<Weakness> getALl() {
 		return this.weaknessRepository.findAll();
@@ -23,6 +28,18 @@ public class WeaknessService {
 		if (weakness == null) {
 			Weakness newWeakness = new Weakness(rate);
 			this.weaknessRepository.save(newWeakness);
+		}
+	}
+
+	public void saveWeaknessHasType(String rate, String name) {
+		Weakness weakness = this.weaknessRepository.findOneByRate(rate);
+		Type type = this.typeRepository.findOneByName(name);
+
+		if (weakness != null && type != null) {
+			if (!weakness.getTypesWeakness().contains(type)) {
+				weakness.getTypesWeakness().add(type);
+				this.weaknessRepository.save(weakness);
+			}
 		}
 	}
 }
